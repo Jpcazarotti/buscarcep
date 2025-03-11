6b8599eda72c378c1e9d7214a3a747f94089540d
João Pedro Cazarotti Batisata <joaopedrocazarottijp@gmail.com>
2025-03-11 17:19:23 -0300

    api instalada!

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:buscarcep/menu_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,6 +14,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final TextEditingController _cep =
       TextEditingController(); // O '_' é parar dizer que a variável é privada!
+
+  Map<String, dynamic>? _cepData;
+  bool _isLoading = false;
+
+  Future<void> _buscarCep() async {
+    final String cep = _cep.text.trim();
+    setState(() => _isLoading = true);
+    // {} passou para =>, assim deixa em apenas uma linha o setState.
+    final response =
+        await http.get(Uri.parse("https://viacep.com.br/ws/$cep/json/"));
+    setState(() {
+      _cepData = json.decode(response.body);
+      // print(_cepData);
+    });
+    setState(() => _isLoading = false);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +50,16 @@ class _HomePageState extends State<HomePage> {
             TextField(
               controller: _cep,
               keyboardType: TextInputType.number,
+              inputFormatters: [
+                MaskTextInputFormatter(
+                    mask: '#####-###',
+                    filter: {"#": RegExp(r'[0-9]')},
+                    type: MaskAutoCompletionType.lazy),
+              ],
               decoration: const InputDecoration(
                 labelText: "Digite o CEP",
+                hintText: "00000-000",
+                hintStyle: TextStyle(color: Color(0xFFBDBDBD)),
                 border: OutlineInputBorder(),
               ),
             ),
@@ -40,64 +67,66 @@ class _HomePageState extends State<HomePage> {
               height: 20,
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: _buscarCep,
               child: const Text("Buscar"),
             ),
             const SizedBox(
               height: 5,
             ),
-            const Card(
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Informações do CEP",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+            _isLoading
+                ? const LinearProgressIndicator()
+                : Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Informações do CEP",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "CEP: ${_cepData?["cep"] ?? ""}",
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "Logradouro: ${_cepData?["logradouro"] ?? ""}",
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "Bairro: ${_cepData?["bairro"] ?? ""}",
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "Cidade: ${_cepData?["localidade"] ?? ""}",
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            "Estado: ${_cepData?["uf"] ?? ""}",
+                            style: const TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Text(
-                      "CEP: 00000-000",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Text(
-                      "Logradouro: Rua Exemplo",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Text(
-                      "Bairro: Centro",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Text(
-                      "Cidade: Cidade Exemplo",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    Text(
-                      "Estado: EX",
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
+                  ),
           ],
         ),
       ),
